e99b88388a26de3c9fbb2159fa73d9788b4f82e3
Ronan Murphy <ronan.murphy@originmarkets.com>
2019-12-03 20:23:32 +0000

    Adding new class model etc

--- tickets/models.py
+++ tickets/models.py
@@ -3,14 +3,19 @@ from django.db import models
 
 class Player(models.Model):
     name = models.CharField(max_length=100)
-    color = models.CharField(max_length=6)
 
     def __str__(self):
         return self.name
 
 
+class Class(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=20)
+
+
 class Session(models.Model):
     name = models.CharField(max_length=100)
+    session_class = models.ForeignKey(Class, on_delete=models.CASCADE, related_name="sessions")
     created_at = models.DateTimeField(auto_now_add=True)
     finished_at = models.DateTimeField(null=True, blank=True)
 
